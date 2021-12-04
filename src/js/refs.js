1d78f5340c307b6c771da50b4a62b57cba6cdf04
KseniaLF <llf9391@gmail.com>
2021-12-04 17:50:14 +0200

    c

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,11 +2,12 @@ export const refs = {
     filmsContainerRef: document.querySelector(".films-container"),
     headerButtons: document.querySelector(".header-functional"),
     pageLibrary: document.querySelector(".page-library"),
-
+    headerFunctional: document.querySelector(".header-functional"),
+    pageHome: document.querySelector(".page-home"),
 //////// Временные пока не будет готов header
 /////// временную разметку сделала в Lyuda.html
 ///  использовала для реалтзации запроса поиска кинофильма по ключевому слову на главное странице
-    headerSearchForm: document.querySelector(".search-form"),
-    headerSearchFormInput: document.querySelector(".search-form > input"),
-
+    // headerSearchForm: document.querySelector(".search-form"),
+    // headerSearchFormInput: document.querySelector(".search-form > input"),
+//немного изменила разметку, ссылки получила прямо в index.js
 }
