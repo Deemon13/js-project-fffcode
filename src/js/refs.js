4f1727e8ec80aa485bdabfa93f4d8a3eb0e43ea2
KseniaLF <llf9391@gmail.com>
2021-12-04 18:16:14 +0200

    логика хедера

--- src/js/refs.js
+++ src/js/refs.js
@@ -4,10 +4,4 @@ export const refs = {
     pageLibrary: document.querySelector(".page-library"),
     headerFunctional: document.querySelector(".header-functional"),
     pageHome: document.querySelector(".page-home"),
-//////// Временные пока не будет готов header
-/////// временную разметку сделала в Lyuda.html
-///  использовала для реалтзации запроса поиска кинофильма по ключевому слову на главное странице
-    // headerSearchForm: document.querySelector(".search-form"),
-    // headerSearchFormInput: document.querySelector(".search-form > input"),
-//немного изменила разметку, ссылки получила прямо в index.js
 }
