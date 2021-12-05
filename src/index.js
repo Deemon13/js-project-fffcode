a360a9b0b4aae01a317be5e344ebced0c87f95b1
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-12-05 15:01:22 +0200

    clean-2 of index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import { getGenresArray, transformGenresList } from './js/genres';
 import { modal } from './js/modal';
 
 import Utils from './js/utils';
+import SearchProps from './js/search';
+
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '9eab4199b01913b6a81b6702a89a7ff0';
 
@@ -53,50 +55,14 @@ function onClickPageHome() {
 /// Реализация поиска кинофильма по ключевому слову (на главной странице)
 
 let searchOptionsFromUser = `search/movie`; //для запроса по ключевому слову
-let requestFromUser = '';
 
-document.querySelector('.search-form').addEventListener('submit', checkRequest);
+document.querySelector('.search-form').addEventListener('submit', SearchProps.checkRequest);
 
-async function getSerchFilmsFromUser(requestFromUser) {
+export default async function getSerchFilmsFromUser(requestFromUser) {
   const response = await axios.get(
     `${searchOptionsFromUser}?api_key=${API_KEY}&language=en-US&query=${requestFromUser}&page=1&include_adult=false`,
   );
   return response.data;
-}
-
-// прверяем  то что ввел User
-function checkRequest(event) {
-  event.preventDefault();
-  requestFromUser = document.querySelector('.search-form_input').value;
-  if (!requestFromUser) {
-    console.log('Введите название фильма для поиска, пожалуйста');
-    return;
-  }
-  /// если ОК то делаем запрос
-  onSearchFromUser(requestFromUser);
-}
-
-async function onSearchFromUser(requestFromUser) {
-  // чистим перед отрисовкой результатов поиска
-  Utils.clearFoo();
-
-  try {
-    const response = await getSerchFilmsFromUser(requestFromUser);
-    if (!response.total_results) {
-      console.log(
-        'Извините, фильмов, соответствующих вашему поисковому запросу, нет. Пожалуйста, попробуйте еще раз.',
-      );
-      return;
-    }
-
-    const responseTotalResults = response.total_results; /// Кол-во найденных результатов
-
-    console.log(`We found ${responseTotalResults} movies.`);
-    Utils.renderMarkup(response); /// Рисуем
-  } catch (error) {
-    console.log('что-то пошло не так');
-    return;
-  }
 }
 
 /////////////////////////////////////////////////
