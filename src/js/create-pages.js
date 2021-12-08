5df746c9fcb3903f9d6d746655735c9d0e3852d9
Lesiv-Aleksandr <85366097+Lesiv-Aleksandr@users.noreply.github.com>
2021-12-08 23:18:22 +0200

    btn-styles

--- src/js/create-pages.js
+++ src/js/create-pages.js
@@ -5,8 +5,8 @@ export function createLibrary() {
 refs.header.classList.add('library');
     refs.filmsContainerRef.innerHTML = " "
     refs.headerButtons.innerHTML = `
-        <button type="button" data-action="watched">Watched</button>
-        <button type="button" data-action="queue">queue</button>`
+        <button class="btn btn__hover-focus" type="button" data-action="watched">Watched</button>
+        <button class="btn btn__hover-focus" type="button" data-action="queue">queue</button>`
     createLibraryPlug();
     document.querySelector(".plug-message").textContent = "Add movies to the queue so you don't forget to watch them later!"
 }
