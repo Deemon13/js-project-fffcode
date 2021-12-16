57443e422c052b475522f02129ed2c18d1c08537
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-12-16 05:48:01 +0200

    added youtube-icon

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
-import icons from "/images/icons.svg";
+import icons from '/images/icons.svg';
+import youtubeicon from '/images/youtube.svg';
 export { icons };
-import "./sass/main.scss";
-import "animate.css";
-import API from "./js/api-func";
-import { refs } from "./js/refs";
-import { createLibrary } from "./js/create-pages";
-import { createHome } from "./js/create-pages";
-import { getGenresArray, transformGenresList } from "./js/genres";
-import { modal } from "./js/modal";
-import { listenModalClick, onGalleryModalOpen } from "./js/modal";
-import { onCloseModal, onClickBtnQueue, onClickBtnWatched } from "./js/modal";
-import Utils from "./js/utils";
-import SearchProps from "./js/search";
-import { initPagination } from "./js/pagination";
-import { renderQueue, renderWatched } from "./js/render-library";
-import { logo } from "./js/ligo";
-import scrollUp from "./js/scrollUp";
-import scrollDown from "./js/scrollDown";
-import { sortByGenres, showGenresFilter, hideGenresFilter } from "./js/genres-filter";
+export { youtubeicon };
+import './sass/main.scss';
+import 'animate.css';
+import API from './js/api-func';
+import { refs } from './js/refs';
+import { createLibrary } from './js/create-pages';
+import { createHome } from './js/create-pages';
+import { getGenresArray, transformGenresList } from './js/genres';
+import { modal } from './js/modal';
+import { listenModalClick, onGalleryModalOpen } from './js/modal';
+import { onCloseModal, onClickBtnQueue, onClickBtnWatched } from './js/modal';
+import Utils from './js/utils';
+import SearchProps from './js/search';
+import { initPagination } from './js/pagination';
+import { renderQueue, renderWatched } from './js/render-library';
+import { logo } from './js/ligo';
+import scrollUp from './js/scrollUp';
+import scrollDown from './js/scrollDown';
+import { sortByGenres, showGenresFilter, hideGenresFilter } from './js/genres-filter';
 
-const settings = { page: 1, type: "popular-films" };
+const settings = { page: 1, type: 'popular-films' };
 export { settings };
 
 let pagination = null;
 
 // export const LOCALSTORAGE_ARR_MOVIES = "arr-current-movies";
 
-API.getPopularFilms().then((results) => {
+API.getPopularFilms().then(results => {
   Utils.spinner();
   const { page, total_results: totalResults } = results;
   getGenresArray(Utils.genresName);
@@ -45,32 +47,32 @@ API.getPopularFilms().then((results) => {
 createHome();
 sortByGenres();
 
-refs.pageLibrary.addEventListener("click", onClickPageLibrary); //слушатель на кнопке библиотеки
+refs.pageLibrary.addEventListener('click', onClickPageLibrary); //слушатель на кнопке библиотеки
 
 function onClickPageLibrary() {
-  refs.pageHome.classList.add("page-home__color-ack");
-  refs.pageHome.classList.remove("header__link_current");
-  refs.pageLibrary.classList.add("header__link_current");
+  refs.pageHome.classList.add('page-home__color-ack');
+  refs.pageHome.classList.remove('header__link_current');
+  refs.pageLibrary.classList.add('header__link_current');
   // imgHero.srs = ''
   hideGenresFilter();
   createLibrary(); //рендер кнопок на странице библиотеки
 
-  document.querySelector("[data-action='watched']").addEventListener("click", onClickBtnWatched);
-  document.querySelector("[data-action='queue']").addEventListener("click", onClickBtnQueue);
-  document.querySelector("[data-action='queue']").classList.toggle("activeBtnEl");
+  document.querySelector("[data-action='watched']").addEventListener('click', onClickBtnWatched);
+  document.querySelector("[data-action='queue']").addEventListener('click', onClickBtnQueue);
+  document.querySelector("[data-action='queue']").classList.toggle('activeBtnEl');
 
-  refs.pageHome.addEventListener("click", onClickPageHome);
+  refs.pageHome.addEventListener('click', onClickPageHome);
   const queueMovies = renderQueue();
   if (!queueMovies) {
-    pagination.then((res) => {
-      settings.type = "queue";
+    pagination.then(res => {
+      settings.type = 'queue';
       res.reset(0);
       res.movePageTo(1);
     });
     return;
   }
-  pagination.then((res) => {
-    settings.type = "queue";
+  pagination.then(res => {
+    settings.type = 'queue';
     res.reset(queueMovies.length);
     res.movePageTo(1);
   });
@@ -78,9 +80,9 @@ function onClickPageLibrary() {
 
 // refs.pageHome.classList.add('page-home__color-ack');
 export async function onClickPageHome() {
-  refs.pageHome.classList.remove("page-home__color-ack");
-  refs.pageLibrary.classList.remove("header__link_current");
-  refs.pageHome.classList.add("header__link_current");
+  refs.pageHome.classList.remove('page-home__color-ack');
+  refs.pageLibrary.classList.remove('header__link_current');
+  refs.pageHome.classList.add('header__link_current');
 
   createHome(); //рендер кнопок на главной странице
   showGenresFilter();
@@ -90,36 +92,36 @@ export async function onClickPageHome() {
   Utils.renderMarkup(data);
 
   listenModalClick(onGalleryModalOpen);
-  refs.pageLibrary.addEventListener("click", onClickPageLibrary);
-  refs.pageHome.removeEventListener("click", onClickPageHome);
-  pagination.then((res) => {
-    settings.type = "popular-films";
+  refs.pageLibrary.addEventListener('click', onClickPageLibrary);
+  refs.pageHome.removeEventListener('click', onClickPageHome);
+  pagination.then(res => {
+    settings.type = 'popular-films';
     res.reset(data.total_pages);
     res.movePageTo(1);
   });
 }
-const logoHome = document.querySelector(".header__logo");
-logoHome.addEventListener("click", onClickLogo);
+const logoHome = document.querySelector('.header__logo');
+logoHome.addEventListener('click', onClickLogo);
 function onClickLogo(e) {
   onClickPageHome();
 }
 
 /// Реализация поиска кинофильма по ключевому слову (на главной странице)
 
-document.querySelector(".search-form").addEventListener("submit", SearchProps.checkRequest);
+document.querySelector('.search-form').addEventListener('submit', SearchProps.checkRequest);
 
 /////////////////////////////////////////////////
 
 export { pagination };
 export function saveArrMoviesToLocalStorage(arrMovies) {
-  localStorage.setItem("arr-current-movies", JSON.stringify(arrMovies)); // сохраняем в локал данные про фильмы
+  localStorage.setItem('arr-current-movies', JSON.stringify(arrMovies)); // сохраняем в локал данные про фильмы
 }
 export function getArrMoviesFromLocalStorage() {
-  const savedArrMovies = localStorage.getItem("arr-current-movies");
+  const savedArrMovies = localStorage.getItem('arr-current-movies');
   return JSON.parse(savedArrMovies); // получаем данные про фильмы с локала
 }
 
-refs.openTeamBtn.addEventListener("click", openTeamModal);
+refs.openTeamBtn.addEventListener('click', openTeamModal);
 
 function openTeamModal(event) {
   event.preventDefault();
@@ -130,7 +132,7 @@ function openTeamModal(event) {
 }
 
 function toggleTeam() {
-  refs.team.classList.toggle("is-hidden");
+  refs.team.classList.toggle('is-hidden');
 }
 
 function onTeamModalClose() {
